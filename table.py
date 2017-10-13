b1fc22cb5d551893cd0c6446625d528de67a4fb1
Harris Partners <tech@harris.partners>
2017-10-13 16:45:44 +1100

    updated

--- table.py
+++ table.py
@@ -2,6 +2,8 @@ import boto3	#Used for AWS... Not sure if required.
 import MySQLdb 	#Need for connection to SQL db
 import random	#Need for random id generation
 import string	#Need for random id generation
+import datetime
+import time
 
 conn = MySQLdb.connect(host="harrisfaceapi.ckuvqwkjly5s.ap-southeast-2.rds.amazonaws.com", port=3306, user="harris", passwd="988$_iADO_k9484ASDJFSDJ_afdsj", db="harris_face_dev")
 cur = conn.cursor() #Create a cursor for the select
@@ -10,7 +12,7 @@ print("You have just connected to the database! You are a true legend!")
 cur.execute("SELECT * FROM api_customer")
 results = cur.fetchall()
 
-#Create a table for print function
+# Create a table for print function
 widths = []
 columns = []
 tavnit = '|'
@@ -24,7 +26,7 @@ for w in widths:
     tavnit += " %-"+"%ss |" % (w,)
     separator += '-'*w + '--+'
 
-#Print the results of the SQL Table in a table format
+# Print the results of the SQL Table in a table format
 print(separator)
 print(tavnit % tuple(columns))
 print(separator)
@@ -33,7 +35,7 @@ for row in results:
 	print(tavnit % row)
 print(separator)
 
-#Display all the names of people we know and exclude face_ids missing both first name or last name.
+# Display all the names of people we know and exclude face_ids missing both first name or last name.
 print("Here is a list of people whose face we have stored in Harris 360.")
 
 cursor = conn.cursor(MySQLdb.cursors.DictCursor)
@@ -42,8 +44,28 @@ result_set = cursor.fetchall()
 for row in result_set:
     print "%s, %s, %s" % (row["face_id"], row["first_name"], row["last_name"])
 
-#Add new entry
+############################ WORK IN PROGRESS ##############################
 
+# Add new entry to database - Option 1
+'''def new_entry():
+	cur = conn.cursor()
+	cur.execute("INSERT INTO api_customer VALUES (80, '2017-10-13 01:46:00', 1, 1, 1, 1, 'Eman', 'Test', 1, 1, 1)")
+
+new_entry()'''
+
+# Add new entry to database - Option 2
+
+def dynamic_data_entry():
+	unix = time.time()
+	date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+	keyword = 'Python'
+	value = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+	cur.execute("INSERT INTO api_customer (id, created, first_name, last_name) VALUES (%s, %s, %s, %s)", (id, date, keyword, value))
+	conn.commit()
+
+dynamic_data_entry()
+
+############################ WORK IN PROGRESS ##############################
 
 
 #Line below creates a unique ID - Need to work out where to put this.
